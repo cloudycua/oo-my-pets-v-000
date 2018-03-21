901ec4c596e25177813c153d38024f48a93a55bd
Christine Cua <cloudycua@gmail.com>
2018-03-21 18:20:35 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,19 +1,19 @@
 class Owner
-  attr_reader :species :pets
-  attr_accessor :name
-  @@all = []
+  #attr_reader :species :pets
+  #attr_accessor :name
+  #@@all = []
 
-  def initialize(species)
-    @species = species
-    @pets = {fishes: [], cats: [], dogs: []}
-  end
+  #def initialize(species)
+  #  @species = species
+  #  @pets = {fishes: [], cats: [], dogs: []}
+  #end
 
-  def self.all
-    @@all
-  end
-
-  def say_species
-    "I am a #{species}."
-  end
+  #def self.all
+  #  @@all
+  #end
+#
+#  def say_species
+#    "I am a #{species}."
+#  end
 
 end
