0847d8ae7b39a0c389de0907544d3f690a188958
Christine Cua <cloudycua@gmail.com>
2018-03-21 18:17:40 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,13 @@
 class Owner
-  # code goes here
+  attr_reader :species :pet
+  attr_accessor :name
+
+  def initialize(species)
+    @species = species
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
 end
